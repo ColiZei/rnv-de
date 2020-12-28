bd3a372dee1469c224bcbf36ad14d6d781847e28
Sebastian Collazo-Quaas <mission@collazo.de>
2020-12-28 10:25:27 +0100

    Added basic component and route

---
+++ prototyp/frontend-prototype/src/views/Map.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <p>Map!</p>
+  </v-container>
+</template>
+
+<script>
+// @ is an alias to /src
+// import Map from '@/components/Map.vue';
+
+// export default {
+//   name: 'Map',
+//   components: {
+//     Map,
+//   },
+// };
+</script>
