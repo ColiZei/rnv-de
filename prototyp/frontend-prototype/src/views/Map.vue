3c3af6cce7b2607e8f3f85998cbb117f6d8abfad
Sebastian Collazo-Quaas <mission@collazo.de>
2020-12-28 22:54:26 +0100

    Frontend: Added map data to store

--- prototyp/frontend-prototype/src/views/Map.vue
+++ prototyp/frontend-prototype/src/views/Map.vue
@@ -47,7 +47,11 @@
 </template>
 
 <script>
-import { latLng, Icon } from 'leaflet';
+import {
+  latLng,
+  // Icon
+} from 'leaflet';
+
 import {
   LMap,
   LTileLayer,
@@ -63,146 +67,13 @@ import 'leaflet/dist/leaflet.css';
 // Fix for Webpack: https://vue2-leaflet.netlify.app/quickstart/#marker-icons-are-missing
 // Requires Icon import from leaflet
 /* eslint-disable no-underscore-dangle */
-delete Icon.Default.prototype._getIconUrl;
-Icon.Default.mergeOptions({
-  /* eslint-disable global-require */
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-});
-
-const markersVegan = [
-  {
-    id: 'm1',
-    position: { lat: 51.505, lng: -0.09 },
-    tooltip: 'tooltip for marker1',
-    draggable: true,
-    visible: true,
-    locationType: 'cafe',
-  },
-  {
-    id: 'm2',
-    position: { lat: 51.8905, lng: -0.09 },
-    tooltip: 'tooltip for marker2',
-    draggable: true,
-    visible: true,
-    locationType: 'restaurant',
-  },
-  {
-    id: 'm3',
-    position: { lat: 51.005, lng: -0.09 },
-    tooltip: 'tooltip for marker3',
-    draggable: true,
-    visible: true,
-    locationType: 'cafe',
-  },
-  {
-    id: 'm4',
-    position: { lat: 51.7605, lng: -0.09 },
-    tooltip: 'tooltip for marker4',
-    draggable: true,
-    visible: true,
-    locationType: 'icecream',
-  },
-];
-
-const markersOmni = [
-  {
-    id: 'm9',
-    position: { lat: 51.505, lng: -0.29 },
-    tooltip: 'tooltip for marker5',
-    draggable: true,
-    visible: true,
-    locationType: 'cafe',
-  },
-  {
-    id: 'm10',
-    position: { lat: 51.8905, lng: -0.29 },
-    tooltip: 'tooltip for marker6',
-    draggable: true,
-    visible: true,
-    locationType: 'icecream',
-  },
-  {
-    id: 'm11',
-    position: { lat: 51.005, lng: -0.29 },
-    tooltip: 'tooltip for marker7',
-    draggable: true,
-    visible: true,
-    locationType: 'restaurant',
-  },
-  {
-    id: 'm12',
-    position: { lat: 50.7605, lng: -0.29 },
-    tooltip: 'tooltip for marker8',
-    draggable: true,
-    visible: true,
-    locationType: 'restaurant',
-  },
-];
-
-const markersVeggi = [
-  {
-    id: 'm5',
-    position: { lat: 51.505, lng: -0.19 },
-    tooltip: 'tooltip for marker5',
-    draggable: true,
-    visible: true,
-    locationType: 'icecream',
-  },
-  {
-    id: 'm6',
-    position: { lat: 51.8905, lng: -0.19 },
-    tooltip: 'tooltip for marker6',
-    draggable: true,
-    visible: true,
-    locationType: 'restaurant',
-  },
-  {
-    id: 'm7',
-    position: { lat: 51.005, lng: -0.19 },
-    tooltip: 'tooltip for marker7',
-    draggable: true,
-    visible: true,
-    locationType: 'restaurant',
-  },
-  {
-    id: 'm8',
-    position: { lat: 50.7605, lng: -0.19 },
-    tooltip: 'tooltip for marker8',
-    draggable: true,
-    visible: true,
-    locationType: 'restaurant',
-  },
-];
-
-//  Gruppe aller Marker
-const markers = [
-  {
-    id: 'vegan',
-    name: 'Vegan',
-    markers: markersVegan,
-    visible: true,
-    markersVisible: true,
-    iconColor: 'green',
-  },
-  {
-    id: 'veggi',
-    name: 'Veggi',
-    markers: markersVeggi,
-    visible: true,
-    markersVisible: true,
-    iconColor: 'blue',
-  },
-  {
-    id: 'omni',
-    name: 'Omni',
-    markers: markersOmni,
-    visible: true,
-    markersVisible: true,
-    iconColor: 'red',
-  },
-];
+// delete Icon.Default.prototype._getIconUrl;
+// Icon.Default.mergeOptions({
+//   /* eslint-disable global-require */
+//   iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+//   iconUrl: require('leaflet/dist/images/marker-icon.png'),
+//   shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+// });
 
 export default {
   name: 'Map',
@@ -220,7 +91,6 @@ export default {
     return {
       zoom: 9,
       center: [51.505, -0.09],
-      // center: latLng(49.4467, 8.5975),
       url: `https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=${process.env.VUE_APP_MAPBOX_API_ACCESSTOKEN}`,
       attribution:
         'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -229,16 +99,13 @@ export default {
       mapOptions: {
         zoomSnap: 0.5,
       },
-      markerGroup: markers,
+      markerGroup: [],
     };
   },
-  computed: {
-    iconName() {
-      // console.log(locationType);
-      return 'mdi-plus';
-    },
-  },
   methods: {
+    loadMarkers() {
+      this.markerGroup = this.$store.getters['map/getMarkers'];
+    },
     getIconName(locationType) {
       let iconName = 'mdi-silverware';
       if (locationType === 'icecream') {
@@ -258,6 +125,9 @@ export default {
     // showMarkerDetails(marker) {
     //   console.log(marker);
     // },
+  },
+  created() {
+    this.loadMarkers();
   },
 };
 </script>
